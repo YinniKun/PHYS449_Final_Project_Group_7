3fb8daec977454d51164b175d760a7771380ab62
UsingNamespaceNolan <nolan.johnston335@gmail.com>
2022-11-18 14:17:40 -0500

    implemented loss train times

--- glycolysis/main.py
+++ glycolysis/main.py
@@ -16,13 +16,25 @@ def run_nn(param, model, data):
     :return: ?
     """
     learning_rate = param['optim']['learning_rate']
+    data_loss_train_size = param['data']['num_data_loss']
+    ode_loss_train_size = param['data']['num_ode_loss']  # cant find in the paper where they specify this set size
 
 
     # Define an optimizer and the loss function
-    optimizer = optim.Adam(model.parameters(), lr=param['learning_rate'])
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     mean_square_loss = torch.nn.MSELoss(reduction= 'mean')
 
     model.reset()  # reset model parameters every time the nn is run
+
+    # get data_loss_train_size random time points for training the data loss
+    rand_indexes = np.random.default_rng().choice(data.conc[:, 0].size, data_loss_train_size, replace=False)
+    rand_indexes = np.sort(rand_indexes)
+    data_loss_train_times = data.conc[rand_indexes, 0]
+
+    # get ode_loss_train_size equispaced time points for training the ode loss
+    ode_loss_indexes = np.round(np.linspace(0, len(data.conc[:, 0]) - 1, ode_loss_train_size)).astype(int)
+    ode_loss_train_times = data.conc[ode_loss_indexes, 0]
+
 
 
 
@@ -37,7 +49,7 @@ if __name__ == '__main__':
 
     num_data_points = params['data']['num_data_points']
 
-    data = get_data.Data(n_points=args.data_points)
+    data = get_data.Data(n_points=num_data_points)
     data.save_as_csv()
 
     model = Net(1)  # need to figure out the parameters to send to this, using 1 as dummy parameter
