4a2c311b43d04749373b36420d7621647c546d49
Callum Follett <callumf2011@gmail.com>
2022-11-21 09:31:46 -0500

    small changes main.py, added species loops to main.py, added num_species_tot and num_species_measured to param.json

--- glycolysis/main.py
+++ glycolysis/main.py
@@ -1,10 +1,16 @@
-#main file for glycolosis
+# main file for glycolysis
 
-import json, argparse, torch, sys, math
-import src.data_gen as get_data
+import argparse
+import json
+import math
+import torch
+
 import numpy as np
 import torch.optim as optim
+
+import src.data_gen as get_data
 from src.nn_gen import Net
+
 
 def calculate_weights(loss_values):
     """
@@ -24,7 +30,9 @@ def calculate_weights(loss_values):
         return weights[0]
     else:
         return weights
-def weighted_loss(input, label, loss):
+
+
+def weighted_loss(input, label, loss, model):
     """
     calculates weighted loss for any set of num
     :param input: 1D Pytorch tensor
@@ -32,11 +40,12 @@ def weighted_loss(input, label, loss):
     :param loss: Pytorch tensor
     :return: 1D Pytorch tensor
     """
-    loss_value = loss(Net.forward(input), label)
+    loss_value = loss(model.forward(input), label)
     loss_value = loss_value / input.size()
     weight = calculate_weights(loss_value)
     loss_value = loss_value * weight
     return loss_value
+
 
 def run_nn(param, model, data):
     """
@@ -53,18 +62,23 @@ def run_nn(param, model, data):
 
                                     ############ CALLUM START #############
 
-    # Statements for noisy/noiseless flag, make the noisy/noiseless part of the .json and include in data param??
-    if param['optim']['noisy?'] == False:
-        epoch_init_iter = param['optim']['init_iters_noiseless']
-        epoch_full_iter = param['optim']['full_iters_noiseless']
+    s = param['data']['num_species_tot']
+    m = param['data']['num_species_measured']
 
-    elif param['optim']['noisy?'] == True:
+    # should put a flag somewhere (maybe in the Data class) to indicate if the data is noiseless or noisy
+    # Statements for noisy/noiseless flag, make the noisy/noiseless part of the .json and include in data param??
+    if param['optim']['noisy?']:
         epoch_init_iter = param['optim']['init_iters_noisy']
         epoch_full_iter = param['optim']['full_iters_noisy']
+
+    elif not param['optim']['noisy?']:
+        epoch_init_iter = param['optim']['init_iters_noiseless']
+        epoch_full_iter = param['optim']['full_iters_noiseless']
 
     else:
         print("cannot determine if noisy or noiseless")
         exit()
+
 
                                         ############ CALLUM FINISH #############
 
@@ -90,58 +104,67 @@ def run_nn(param, model, data):
                                     ############ CALLUM START #############
 
     # loop through initial iterations training just data and aux loss
-    print("Initial Data and Aux Training:")
     for x in range(0, epoch_init_iter-1):
         losses_data = []
+        losses_aux = []
 
-        # calculate data loss
-        for inc, y in enumerate(data.data_inputs):
-            loss_data = weighted_loss(y, data.data_labels[inc], mean_square_loss)
-            losses_data = losses_data.append(loss_data)
+        # calculate data loss for measures species
+        for y in range(0, m):
+            for inc, z in enumerate(data.data_inputs):
+                loss_data = weighted_loss(y, data.data_labels[inc], mean_square_loss, model)
+                losses_data = losses_data.append(loss_data)
 
-        # calculate auxiliary loss
-        for inc, y in enumerate(data.aux_inputs):
-            loss_aux = weighted_loss(inc, data.aux_labels[y], mean_square_loss)
-            losses_aux = losses_aux.append(loss_aux)
-        loss_tot = loss_data + loss_aux
+        # calculate auxiliary loss for all species
+        for y in range(0, s):
+            for inc, z in enumerate(data.aux_inputs):
+                loss_aux = weighted_loss(inc, data.aux_labels[y], mean_square_loss, model)
+                losses_aux = losses_aux.append(loss_aux)
+
+        loss_tot = losses_data.sum() + losses_aux.sum()
         Net.backprop(loss_tot, optimizer)
 
         # Print loss 10 times per training session
         if not x % (epoch_init_iter / 10):
-            print(f"Epoch {x}/{epoch_init_iter}: \n loss: {loss_tot} \n\n")
+            print(f"Initial Data and Aux Training:\t \
+            Epoch {x}/{epoch_init_iter}: \t loss: {loss_tot} \n")
         else:
             pass
+    print(f"Initial Data and Aux Training:\t Done")
 
     # then loop through full iterations training all loss
-    print("Full Training:")
     for x in range(0, epoch_full_iter - 1):
         losses_data = []
         losses_ode = []
+        losses_aux = []
 
-        # calculate data loss
-        for inc, y in enumerate(data.data_inputs):
-            loss_data = weighted_loss(y, data.data_labels[inc], mean_square_loss)
-            losses_data = losses_data.append(loss_data)
+        # calculate data loss for measured species
+        for y in range(0, m):
+            for inc, z in enumerate(data.data_inputs):
+                loss_data = weighted_loss(y, data.data_labels[inc], mean_square_loss, model)
+                losses_data = losses_data.append(loss_data)
 
-        # calculate ode loss
-        for inc, y in enumerate(data.ode_state):
-            loss_ode = weighted_loss(y, data.state_derivative[inc], mean_square_loss)
-            losses_ode = losses_ode.append(loss_ode)
+        # calculate ode loss for all species
+        for y in range(0, s):
+            for inc, z in enumerate(data.ode_state):
+                loss_ode = weighted_loss(y, data.state_derivative[inc], mean_square_loss, model)
+                losses_ode = losses_ode.append(loss_ode)
 
-        # calculate auxiliary loss
-        for inc, y in enumerate(data.aux_inputs):
-            loss_aux = weighted_loss(inc, data.aux_labels[y], mean_square_loss)
-            losses_aux = losses_aux.append(loss_aux)
-        loss_tot = loss_data + loss_ode + loss_aux
+        # calculate auxiliary loss for all species
+        for y in range(0, s):
+            for inc, z in enumerate(data.aux_inputs):
+                loss_aux = weighted_loss(y, data.aux_labels[inc], mean_square_loss, model)
+                losses_aux = losses_aux.append(loss_aux)
+
+        loss_tot = losses_data.sum() + loss_ode.sum() + loss_aux.sum()
         Net.backprop(loss_tot, optimizer)
 
         # Print loss 10 times per training session
         if not x % (epoch_full_iter/10):
-            print(f"Epoch {x}/{epoch_full_iter}: \n loss: {loss_tot}")
+            print(f"Full Training: \t \
+            Epoch {x}/{epoch_full_iter}:\t loss: {loss_tot}\n")
         else:
             pass
-    # should put a flag somewhere (maybe in the Data class) to indicate if the data is noiseless or noisy
-    # need to replace data.data_inputs, data.data_labels, data.ode_state, etc. with actual data values
+    # need to replace data.data_inputs, data.data_labels, data.ode_state, etc. with actual data variables
 
                                     ############ CALLUM FINISH #############
 
@@ -160,7 +183,9 @@ if __name__ == '__main__':
     data = get_data.Data(n_points=num_data_points)
     data.save_as_csv()
 
-    model = Net(1)  # need to figure out the parameters to send to this, using 1 as dummy parameter
+    model = Net(1)  # need to figure out the parameters to send to this, using 1 as dummy parameter.
+                    # Should it be 128, because NN width in paper is 128 for glycolysis?
+                    # also where do the 7 feature layers fit in?
 
     run_nn(params, model, data)
 
