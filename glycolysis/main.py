cf179a2af17f76738afb16d1015f88ae5ce9d25d
Callum Follett <callumf2011@gmail.com>
2022-11-21 00:52:02 -0500

    - added training loops for both sets of loss calculations, need to replace data variables in loops, haven't run - added "noisy?" param to param.json for flag for run_nn and potentially data arg

--- glycolysis/main.py
+++ glycolysis/main.py
@@ -1,11 +1,42 @@
 #main file for glycolosis
 
-import json, argparse, torch, sys
+import json, argparse, torch, sys, math
 import src.data_gen as get_data
 import numpy as np
 import torch.optim as optim
 from src.nn_gen import Net
 
+def calculate_weights(loss_values):
+    """
+    Returns a weight that scales the input loss to the order of 1 (10^0)
+    :param loss_values: 1D numpy array
+    :return: scalar value if len(loss_values)=1, 1D numpy array otherwise
+    """
+    loss_values = np.asarray(loss_values)
+    if np.any(loss_values < 0):
+        raise ValueError("Loss values should be non-negative")
+
+    # find smallest order of magnitude in list of losses
+    loss_magnitudes = [math.floor(math.log(loss, 10)) for loss in loss_values]
+    weights = np.asarray([10 ** x for x in loss_magnitudes])
+
+    if len(weights) == 1:
+        return weights[0]
+    else:
+        return weights
+def weighted_loss(input, label, loss):
+    """
+    calculates weighted loss for any set of num
+    :param input: 1D Pytorch tensor
+    :param label: 1D Pytorch tensor
+    :param loss: Pytorch tensor
+    :return: 1D Pytorch tensor
+    """
+    loss_value = loss(Net.forward(input), label)
+    loss_value = loss_value / input.size()
+    weight = calculate_weights(loss_value)
+    loss_value = loss_value * weight
+    return loss_value
 
 def run_nn(param, model, data):
     """
@@ -18,6 +49,24 @@ def run_nn(param, model, data):
     learning_rate = param['optim']['learning_rate']
     data_loss_train_size = param['data']['num_data_loss']
     ode_loss_train_size = param['data']['num_ode_loss']  # cant find in the paper where they specify this set size
+
+
+                                    ############ CALLUM START #############
+
+    # Statements for noisy/noiseless flag, make the noisy/noiseless part of the .json and include in data param??
+    if param['optim']['noisy?'] == False:
+        epoch_init_iter = param['optim']['init_iters_noiseless']
+        epoch_full_iter = param['optim']['full_iters_noiseless']
+
+    elif param['optim']['noisy?'] == True:
+        epoch_init_iter = param['optim']['init_iters_noisy']
+        epoch_full_iter = param['optim']['full_iters_noisy']
+
+    else:
+        print("cannot determine if noisy or noiseless")
+        exit()
+
+                                        ############ CALLUM FINISH #############
 
 
     # Define an optimizer and the loss function
@@ -37,13 +86,64 @@ def run_nn(param, model, data):
 
     aux_loss_train_times = np.asarray([data.conc[0, 0], data.conc[-1, 0]])
 
+
+                                    ############ CALLUM START #############
+
     # loop through initial iterations training just data and aux loss
+    print("Initial Data and Aux Training:")
+    for x in range(0, epoch_init_iter-1):
+        losses_data = []
+
+        # calculate data loss
+        for inc, y in enumerate(data.data_inputs):
+            loss_data = weighted_loss(y, data.data_labels[inc], mean_square_loss)
+            losses_data = losses_data.append(loss_data)
+
+        # calculate auxiliary loss
+        for inc, y in enumerate(data.aux_inputs):
+            loss_aux = weighted_loss(inc, data.aux_labels[y], mean_square_loss)
+            losses_aux = losses_aux.append(loss_aux)
+        loss_tot = loss_data + loss_aux
+        Net.backprop(loss_tot, optimizer)
+
+        # Print loss 10 times per training session
+        if not x % (epoch_init_iter / 10):
+            print(f"Epoch {x}/{epoch_init_iter}: \n loss: {loss_tot} \n\n")
+        else:
+            pass
 
     # then loop through full iterations training all loss
+    print("Full Training:")
+    for x in range(0, epoch_full_iter - 1):
+        losses_data = []
+        losses_ode = []
 
+        # calculate data loss
+        for inc, y in enumerate(data.data_inputs):
+            loss_data = weighted_loss(y, data.data_labels[inc], mean_square_loss)
+            losses_data = losses_data.append(loss_data)
+
+        # calculate ode loss
+        for inc, y in enumerate(data.ode_state):
+            loss_ode = weighted_loss(y, data.state_derivative[inc], mean_square_loss)
+            losses_ode = losses_ode.append(loss_ode)
+
+        # calculate auxiliary loss
+        for inc, y in enumerate(data.aux_inputs):
+            loss_aux = weighted_loss(inc, data.aux_labels[y], mean_square_loss)
+            losses_aux = losses_aux.append(loss_aux)
+        loss_tot = loss_data + loss_ode + loss_aux
+        Net.backprop(loss_tot, optimizer)
+
+        # Print loss 10 times per training session
+        if not x % (epoch_full_iter/10):
+            print(f"Epoch {x}/{epoch_full_iter}: \n loss: {loss_tot}")
+        else:
+            pass
     # should put a flag somewhere (maybe in the Data class) to indicate if the data is noiseless or noisy
+    # need to replace data.data_inputs, data.data_labels, data.ode_state, etc. with actual data values
 
-
+                                    ############ CALLUM FINISH #############
 
 
 if __name__ == '__main__':
--- glycolysis/src/nn_gen.py
+++ glycolysis/src/nn_gen.py
@@ -39,21 +39,14 @@ class Net(nn.Module):
         self.fc4.reset_parameters()
 
     # Backpropagation function
-     # ode_mean (output scaling) is a the the magnitudes of the mean values of the ODE solution, in a vector
-    #   ode_mean should be a 1xn vector (callum and nolan think it should be in main.py)
-    #!!!!!! the loss function is yet to be defined in main.py !!!!!
-    def backprop(self, data, loss, optimizer):
+    def backprop(self, loss_val, optimizer):
         self.train()
-        loss_data = self.weighted_loss(data.data_inputs, data.data_labels, loss)
-        loss_ode = self.weighted_loss(data.ode_state, data.state_derivative, loss)
-        loss_aux = self.weighted_loss(data.aux_inputs, data.aux_labels, loss)
-        obj_val = loss_data+loss_ode+loss_aux
         optimizer.zero_grad()
-        obj_val.backward()
+        loss_val.backward()
         optimizer.step()
-        return obj_val.item()
+        # return obj_val.item()
 
-    def backprop_no_ode(self, data, loss, optimizer):
+    """def backprop_no_ode(self, data, loss, optimizer):
         self.train()
         loss_data = self.weighted_loss(data.data_inputs, data.data_labels, loss)
         loss_aux = self.weighted_loss(data.aux_inputs, data.aux_labels, loss)
@@ -61,7 +54,7 @@ class Net(nn.Module):
         optimizer.zero_grad()
         obj_val.backward()
         optimizer.step()
-        return obj_val.item()
+        return obj_val.item()""" # func doesn't need to exist, since loss is now passed as argument
 
     # Test function. Avoids calculation of gradients.
 
@@ -70,41 +63,7 @@ class Net(nn.Module):
     def test(self, data, loss, epoch, ode_mean):
         self.eval()
         with torch.no_grad():
-            inputs= torch.from_numpy(data.x_test)
-            targets= torch.from_numpy(data.y_test * ode_mean)
-            cross_val= loss(self.forward(inputs), targets)
+            inputs = torch.from_numpy(data.x_test)
+            targets = torch.from_numpy(data.y_test * ode_mean)
+            cross_val = loss(self.forward(inputs), targets)
         return cross_val.item()
-
-    def weighted_loss(self, input, label, loss):
-        """
-        calculates weighted loss for any set of num
-        :param input: 1D Pytorch tensor
-        :param label: 1D Pytorch tensor
-        :param loss: Pytorch tensor
-        :return: 1D Pytorch tensor
-        """
-        loss_value = loss(self.forward(input), label)
-        loss_value = loss_value/input.size()
-        weight = calculate_weights(loss_value)
-        loss_value = loss_value * weight
-        return loss_value
-
-
-def calculate_weights(loss_values):
-    """
-    Returns a weight that scales the input loss to the order of 1 (10^0)
-    :param loss_values: 1D numpy array
-    :return: scalar value if len(loss_values)=1, 1D numpy array otherwise
-    """
-    loss_values = np.asarray(loss_values)
-    if np.any(loss_values < 0):
-        raise ValueError("Loss values should be non-negative")
-
-    # find smallest order of magnitude in list of losses
-    loss_magnitudes = [math.floor(math.log(loss, 10)) for loss in loss_values]
-    weights = np.asarray([10 ** x for x in loss_magnitudes])
-
-    if len(weights) == 1:
-        return weights[0]
-    else:
-        return weights
