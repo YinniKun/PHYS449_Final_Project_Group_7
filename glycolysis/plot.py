e25931edda6d5ca943b9f2c39fbf0317b53dc1b0
Steph Swanson <sswanson@grhosp.com>
2022-11-28 17:41:33 -0500

    Added plot of loss vs epoch

--- glycolysis/plot.py
+++ glycolysis/plot.py
@@ -1,8 +1,6 @@
 # Plot the glycolysis model.
 
 import os
-import json
-import argparse
 import math
 
 import numpy as np
@@ -15,12 +13,12 @@ import src.data_gen as get_data
 # ****************************************************************************
 
 # File name of predicted p to plot vs true p over epochs.
-do_plot_p_vs_epoch = True
+do_plot_p_vs_epoch = False
 p_file = 'data/p_versus_epochs_11-28 150647.txt'
 
 # File name file of losses to plot over epochs.
-do_plot_loss_vs_epoch = False
-loss_file = ''
+do_plot_loss_vs_epoch = True
+loss_file = 'data/all_losses_11-28 150647.txt'
 
 # File name of concentrations to plot vs measured/true data over time.
 do_plot_conc_from_file = False
@@ -52,8 +50,10 @@ N = 1
 A = 4
 p0 = np.asarray([J0, k1, k2, k3, k4, k5, k6, k, kappa, q, K1, psi, N, A])
 
-# Indices of parameters near 1.
+# Legend names.
 names = ['J_0', 'k_1', 'k_2', 'k_3', 'k_4', 'k_5', 'k_6', 'k', '\kappa', 'q', 'K1', '\psi', 'N', 'A']
+
+# Indices of parameters in value ranges.
 near_0 = [0, 5, 7, 10, 11, 12]
 near_5 = [2, 9, 13]
 near_10 = [3, 6, 8]
@@ -63,8 +63,33 @@ near_100 = [1, 4]
 colours = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red',
            'rebeccapurple', 'tab:pink']
 
-def plot_loss():
-    pass
+
+def plot_loss(loss):
+    """Plot loss values over epochs.
+
+    Plot is named losses.pdf.
+
+    :param p: (num_epochss, K+1) array of predicted parameters and time
+
+    """
+    fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(7, 12),
+                            constrained_layout=True)
+    loss_set = ['Data', 'Auxiliary', 'ODE']
+    for row, name in enumerate(loss_set):
+        axs[row].plot(loss[:, -1], loss[:, row], label=f'{name} loss')
+        # Configure subplot.
+        axs[row].set_xlabel('Epoch')
+        axs[row].set_ylabel('Mean square loss')
+        # Autoscale axes to each subplot.
+        axs[row].relim()
+        axs[row].autoscale()
+        axs[row].legend()
+
+    # Add title.
+    fig.suptitle('Training loss', fontsize=16)
+
+    # Save plot
+    plt.savefig('data/losses.pdf', bbox_inches='tight')
 
 
 def plot_p_vs_true_p(p):
@@ -190,6 +215,20 @@ def get_row_col(ind):
 
 if __name__ == '__main__':
 
+    if do_plot_loss_vs_epoch:
+        if not os.path.isfile(f'{loss_file}'):
+            raise ValueError(f'Cannot find loss_file {loss_file}')
+        # Shape (num_epochs, 4) with last column time (epochs).
+        loss = np.loadtxt(loss_file)
+        plot_loss(loss)
+
+    if do_plot_p_vs_epoch:
+        if not os.path.isfile(f'{p_file}'):
+            raise ValueError(f'Cannot find p_file {p_file}')
+        # Shape (num_epochs, K+1) with last column time (epochs).
+        p = np.loadtxt(p_file)
+        plot_p_vs_true_p(p)
+
     # Only read true concentration once if it is needed.
     # !!!!!!!! Still need to get the measured data with noise somehow !!!!!!!!!!!
     if do_plot_conc_from_file or do_plot_conc_from_p:
@@ -210,10 +249,3 @@ if __name__ == '__main__':
         # Shape (num_data_points, S).
         pred_conc = get_data.glycolysis_model(true_conc[:, 0], p)
         plot_conc_from_p(true_conc, pred_conc)
-
-    if do_plot_p_vs_epoch:
-        if not os.path.isfile(f'{p_file}'):
-            raise ValueError(f'Cannot find p_file {p_file}')
-        # Shape (num_epochs, K+1) with first column time (epochs).
-        p = np.loadtxt(p_file)
-        plot_p_vs_true_p(p)
