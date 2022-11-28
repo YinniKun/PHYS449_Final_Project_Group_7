d517285bc95b7d05e9201e7b98a630a09b46b94b
Steph Swanson <sswanson@grhosp.com>
2022-11-28 17:30:55 -0500

    Added plot of parameter value vs true over epochs

--- glycolysis/plot.py
+++ glycolysis/plot.py
@@ -1,5 +1,6 @@
 # Plot the glycolysis model.
 
+import os
 import json
 import argparse
 import math
@@ -15,7 +16,7 @@ import src.data_gen as get_data
 
 # File name of predicted p to plot vs true p over epochs.
 do_plot_p_vs_epoch = True
-p_file = 'p_versus_epochs_11-28 150647.txt'
+p_file = 'data/p_versus_epochs_11-28 150647.txt'
 
 # File name file of losses to plot over epochs.
 do_plot_loss_vs_epoch = False
@@ -51,20 +52,64 @@ N = 1
 A = 4
 p0 = np.asarray([J0, k1, k2, k3, k4, k5, k6, k, kappa, q, K1, psi, N, A])
 
+# Indices of parameters near 1.
+names = ['J_0', 'k_1', 'k_2', 'k_3', 'k_4', 'k_5', 'k_6', 'k', '\kappa', 'q', 'K1', '\psi', 'N', 'A']
+near_0 = [0, 5, 7, 10, 11, 12]
+near_5 = [2, 9, 13]
+near_10 = [3, 6, 8]
+near_100 = [1, 4]
+
+# Plot colours.
+colours = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red',
+           'rebeccapurple', 'tab:pink']
 
 def plot_loss():
     pass
 
 
-def plot_p_vs_true_p():
-    pass
+def plot_p_vs_true_p(p):
+    """Plot p values vs true p values over epochs.
+
+    Plot is named pred_p_vs_true.pdf.
+
+    :param p: (num_epochss, K+1) array of predicted parameters and time
+
+    """
+    fig, axs = plt.subplots(nrows=4, ncols=1, figsize=(7, 24),
+                            constrained_layout=True)
+    row = 0
+    for p_set in [near_0, near_5, near_10, near_100]:
+        c_num = 0
+        for ind in p_set:
+            axs[row].axhline(y=p0[ind],
+                             color=colours[c_num],
+                             label=r'Exact ${0}={1}$'.format(names[ind], p0[ind]))
+            axs[row].plot(p[:, -1], p[:, ind],
+                          color=colours[c_num], linestyle='dashdot',
+                          label=r'Learned ${0}$'.format(names[ind]))
+            # Configure subplot.
+            axs[row].set_xlabel('Epoch')
+            axs[row].set_ylabel('Parameter value (unitless)')
+            # Autoscale axes to each subplot.
+            axs[row].relim()
+            axs[row].autoscale()
+            c_num += 1
+        axs[row].legend()
+        row += 1
+
+    # Add title.
+    fig.suptitle('Parameter values predicted by network', fontsize=16)
+
+    # Save plot
+    plt.savefig('data/pred_p_vs_true.pdf', bbox_inches='tight')
+
 
 
 def plot_conc_from_file(true_conc, pred_conc):
     """Plot predicted concentrations from p vs measured/true data over time.
 
     Generate one figure with subplots of the concentration of each chemical
-    species. Plot is named pred_conc_from_p_vs_true.pdf.
+    species. Plot is named pred_conc_from_file_vs_true.pdf.
     :param true_conc: (num_data_points, S+1) array of time and true concs
     :param pred_conc: (num_data_points, S+1) array of time and predicted concs
 
@@ -88,7 +133,7 @@ def plot_conc_from_file(true_conc, pred_conc):
         axs[row, col].relim()
         axs[row, col].autoscale()
 
-    # Delete unused bottom right subplot and add legend.
+    # Delete unused bottom right subplot and add legend and title.
     handles, labels = axs[row, col].get_legend_handles_labels()
     col += 1
     fig.delaxes(axs[row, col])
@@ -127,7 +172,7 @@ def plot_conc_from_p(true_conc, pred_conc):
         axs[row, col].relim()
         axs[row, col].autoscale()
 
-    # Delete unused bottom right subplot and add legend.
+    # Delete unused bottom right subplot and add legend and title.
     handles, labels = axs[row, col].get_legend_handles_labels()
     col += 1
     fig.delaxes(axs[row, col])
@@ -135,7 +180,7 @@ def plot_conc_from_p(true_conc, pred_conc):
     fig.suptitle('Analytical concentration from predicted parameters', fontsize=16)
 
     # Save plot
-    plt.savefig('data/pred_conc_from_file_vs_true.pdf', bbox_inches='tight')
+    plt.savefig('data/pred_conc_from_p_vs_true.pdf', bbox_inches='tight')
 
 
 def get_row_col(ind):
@@ -145,17 +190,6 @@ def get_row_col(ind):
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--param', default="src/param.json", help='json parameter file relative path', type=str)
-    parser.add_argument('--data_seed', help='Random seed for generating training data', type=int)
-    args = parser.parse_args()
-
-    with open(args.param) as json_param_file:
-        params = json.load(json_param_file)
-
-    # num_data_points = params['data']['num_data_points']
-    # t = np.linspace(start=0, stop=10, num=num_data_points, dtype=np.float64)
-
     # Only read true concentration once if it is needed.
     # !!!!!!!! Still need to get the measured data with noise somehow !!!!!!!!!!!
     if do_plot_conc_from_file or do_plot_conc_from_p:
@@ -163,6 +197,8 @@ if __name__ == '__main__':
         true_conc = np.loadtxt('data/true_conc.txt', delimiter='\t', skiprows=1)
 
     if do_plot_conc_from_file:
+        if not os.path.isfile(f'{conc_file}'):
+            raise ValueError(f'Cannot find conc_file {conc_file}')
         # Shape (num_data_points, S) with first column time (min).
         pred_conc = np.loadtxt(conc_file)
         # Pass without time axis.
@@ -174,3 +210,10 @@ if __name__ == '__main__':
         # Shape (num_data_points, S).
         pred_conc = get_data.glycolysis_model(true_conc[:, 0], p)
         plot_conc_from_p(true_conc, pred_conc)
+
+    if do_plot_p_vs_epoch:
+        if not os.path.isfile(f'{p_file}'):
+            raise ValueError(f'Cannot find p_file {p_file}')
+        # Shape (num_epochs, K+1) with first column time (epochs).
+        p = np.loadtxt(p_file)
+        plot_p_vs_true_p(p)
